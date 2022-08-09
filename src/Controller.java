5e40270c66bc4c317239e0147330022d12031980
jacqueb <jacqueb@msoe.edu>
2022-08-09 18:10:54 -0500

    started file input

--- src/Controller.java
+++ src/Controller.java
@@ -1,11 +1,15 @@
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -26,9 +30,14 @@ public class Controller implements Initializable {
     }
 
     @FXML
-    public void load() {
-        fileChooser.showOpenDialog(stage);
-        saveMenuItem.setDisable(false);
+    public void load() throws IOException {
+        File file = fileChooser.showOpenDialog(stage);
+        if(file != null){
+            for(String line : Files.readAllLines(file.toPath())){
+                System.out.println(line);
+            }
+            saveMenuItem.setDisable(false);
+        }
     }
 
     @FXML
