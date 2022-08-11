8686e8e6d685b27e21075cb198fd0dc7b4c122c8
jacqueb <jacqueb@msoe.edu>
2022-08-10 19:42:48 -0500

    finished complex class

---
+++ Test/ComplexNumTests.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ComplexNumTests {
+    @Test
+    public void add(){
+        ComplexNum c1 = new ComplexNum(1, 2);
+        ComplexNum c2 = new ComplexNum(3, -4);
+        Assertions.assertEquals("4.0 + -2.0", ComplexNum.add(c1, c2).toString());
+    }
+
+    @Test
+    public void subtract(){
+        ComplexNum c1 = new ComplexNum(1, 2);
+        ComplexNum c2 = new ComplexNum(3, -4);
+        Assertions.assertEquals("-2.0 + 6.0", ComplexNum.subtract(c1, c2).toString());
+    }
+
+    @Test
+    public void multiply(){
+        ComplexNum c1 = new ComplexNum(1, 2);
+        ComplexNum c2 = new ComplexNum(3, -4);
+        Assertions.assertEquals("11.0 + 2.0", ComplexNum.multiply(c1, c2).toString());
+    }
+
+    @Test
+    public void divide(){
+        ComplexNum c1 = new ComplexNum(1, 2);
+        ComplexNum c2 = new ComplexNum(3, -4);
+        Assertions.assertEquals("-0.2 + 0.4", ComplexNum.divide(c1, c2).toString());
+    }
+}
--- src/ComplexNum.java
+++ src/ComplexNum.java
@@ -1,2 +1,41 @@
 public class ComplexNum {
+    private double real;
+    private double imaginary;
+
+    public ComplexNum(double real, double imaginary){
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public static ComplexNum add(ComplexNum num1, ComplexNum num2){
+        return new ComplexNum(num1.real + num2.real, num1.imaginary + num2.imaginary);
+    }
+
+    public static ComplexNum subtract(ComplexNum num1, ComplexNum num2){
+        return new ComplexNum(num1.real - num2.real, num1.imaginary - num2.imaginary);
+    }
+
+    public static ComplexNum multiply(ComplexNum num1, ComplexNum num2){
+        return new ComplexNum((num1.real * num2.real) - (num1.imaginary * num2.imaginary),
+                (num1.real * num2.imaginary) + (num2.real * num1.imaginary));
+    }
+
+    public static ComplexNum divide(ComplexNum num1, ComplexNum num2){
+        ComplexNum temp = multiply(num1, new ComplexNum(num2.real, -1*num2.imaginary));
+        double divisionFactor = (num2.real * num2.real) + (num2.imaginary * num2.imaginary);
+        return new ComplexNum(temp.real/divisionFactor, temp.imaginary/divisionFactor);
+    }
+
+    @Override
+    public String toString(){
+        return real + " + " + imaginary;
+    }
 }
